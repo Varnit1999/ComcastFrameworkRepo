08cadf5b5fb22df27ce48bf5a31c2889fe0b353f
Varnit1999 <bhaskarvarnit@gmail.com>
2026-08-26 11:50:43 +0530

    Modified ProductsPage POM Class

--- ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
+++ ComcastCRMGUIFramework/src/main/java/com/comcast/crm/objectrepositoryutility/ProductsPage.java
@@ -15,26 +15,16 @@ public class ProductsPage {
 
 	@FindBy(xpath = "//img[@title='Create Product...']")
 	private WebElement createNewProductBtn;
-<<<<<<< HEAD
 
 	@FindBy(xpath = "//div[@id='basicsearchcolumns_real']//select[@id='bas_searchfield']")
 	private WebElement productDD;
-=======
-	
-	@FindBy(xpath = "//input[@class='txtBox']")
-	private WebElement searchEdt;
->>>>>>> branch 'main' of https://github.com/Varnit1999/ComcastFrameworkRepo.git
 
 	public WebElement getCreateNewProductBtn() {
 		return createNewProductBtn;
 	}
 
-	public WebElement getSearchEdt() {
-		return searchEdt;
+	public WebElement getProductDD() {
+		return productDD;
 	}
-<<<<<<< HEAD
-=======
-	
->>>>>>> branch 'main' of https://github.com/Varnit1999/ComcastFrameworkRepo.git
 
 }
